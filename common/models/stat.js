1eb3da2b91bbf39a7503bfa9f6939e7b81927356
Owen Auch <owenauch@wustl.edu>
2018-08-04 18:22:25 -0500

    model change

--- common/models/stat.js
+++ common/models/stat.js
@@ -1,5 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = function(Stat) {
-
+  Stat.observe("before save", async function(ctx) {
+    Stat.find(
+      {
+        fields: { id: true },
+        where: {
+          name: ctx.instance.name,
+          week: ctx.instance.week
+        }
+      },
+      (err, dups) => {
+        if (err) {
+          console.log(err);
+        } else {
+          dups.forEach(dup => {
+            Stat.destroyById(dup.id, err => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Duplicate successfully purged: " + ctx.name);
+              }
+            });
+          });
+        }
+      }
+    );
+  });
 };
